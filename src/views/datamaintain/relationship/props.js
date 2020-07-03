1631eda61249362e8dda59f2481c048f843b7c0d
songning0605 <31798032+songning0605@users.noreply.github.com>
2020-07-03 18:44:40 +0800

    修改页面功能

--- src/views/datamaintain/relationship/props.js
+++ src/views/datamaintain/relationship/props.js
@@ -3,62 +3,52 @@ export default {
     inline: true,
     submitBtnText: '查询',
     forms: [
-      { prop: 'name', label: 'Name' },
-      { prop: 'mobile', label: 'Mobile' },
-      { prop: 'testProps', label: '测试属性' },
-      { prop: 'mobilasdfe', label: 'Mobiasdfle' },
-      { prop: 'sex', label: 'Sex', itemType: 'select',
-        options: [
-          { value: '', label: 'All' },
-          { value: 0, label: 'Male' },
-          { value: 1, label: 'Female' }
-        ]
-      },
-      { prop: 'sex', label: 'Sesdfdsfx', itemType: 'select',
-        options: [
-          { value: '', label: 'All' },
-          { value: 0, label: 'Male' },
-          { value: 1, label: 'Female' }
-        ]
-      },
-      { prop: 'sexs', label: 'Sexdsf', itemType: 'select',
-        options: [
-          { value: '', label: 'All' },
-          { value: 0, label: 'Male' },
-          { value: 1, label: 'Female' }
-        ]
-      },
-      { prop: 'sexd', label: 'Ssdex', itemType: 'select',
-        options: [
-          { value: '', label: 'All' },
-          { value: 0, label: 'Male' },
-          { value: 1, label: 'Female' }
-        ]
-      }
+      { prop: 'name', label: 'Asin' },
+      { prop: 'mobile', label: 'FNSku' },
+      { prop: 'testProps', label: 'Seller Sku' },
+      { prop: 'mobilasdfe', label: '创建人' },
+      { prop: 'mobilasdfe', label: '产品名称' }
     ]
   },
   columns: [
     {
       prop: 'name',
-      label: '姓名',
+      label: '期间',
       slotName: 'name'
     },
     // 格式化为字符串
     {
       prop: 'sex',
-      label: '性别'
+      label: 'ASIN'
     },
     {
       prop: 'age',
-      label: '年龄'
+      label: 'Seller Sku'
     },
     {
       prop: 'score',
-      label: '分数'
+      label: 'FNSku'
     },
     {
-      prop: '',
-      label: '操作'
+      prop: 'score',
+      label: '公司型号'
+    },
+    {
+      prop: 'score',
+      label: '产品名称'
+    },
+    {
+      prop: 'score',
+      label: '创建人'
+    },
+    {
+      prop: 'score',
+      label: '创建时间'
+    },
+    {
+      prop: 'operate',
+      label: '操作',
+      slotName: 'operate'
     }
   ]
 }
